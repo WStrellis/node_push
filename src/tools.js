1c085672fdec88226036d7741f7ec92140dce229
Westley Strellis <wstrellis@gmail.com>
2022-03-24 13:10:12 -0400

    formatted

--- src/tools.js
+++ src/tools.js
@@ -7,18 +7,23 @@ function getVapidKeys() {
     const generatedKeys = webpush.generateVAPIDKeys()
 
     const keys = {
-        publicKey: process.env.VAPID_PUBLIC_KEY || "",
-        privateKey: process.env.VAPID_PRIVATE_KEY || "",
+        publicKey: process.env.VAPID_PUBLIC_KEY || '',
+        privateKey: process.env.VAPID_PRIVATE_KEY || '',
     }
 
     if (!keys.publicKey || !keys.privateKey) {
         console.log('Public VAPID key:', keys.publicKey)
-        console.log('Private VAPID key (masked):', "X".repeat(keys.privateKey.length))
-        console.log('Public VAPID key or Private VAPID key not found. You can use these:',generatedKeys)
+        console.log(
+            'Private VAPID key (masked):',
+            'X'.repeat(keys.privateKey.length)
+        )
+        console.log(
+            'Public VAPID key or Private VAPID key not found. You can use these:',
+            generatedKeys
+        )
         return null
     }
 
-    
     return keys
 }
 
