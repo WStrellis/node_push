33ac338c652fac4b0a38fa95721432db7bcce95f
Westley Strellis <wstrellis@gmail.com>
2022-03-29 19:59:10 -0400

    wrote failing test

--- __tests__/endpoints.test.js
+++ __tests__/endpoints.test.js
@@ -46,3 +46,29 @@ describe('tests for subscribe', () => {
         expect(subscriptions.length).toBe(1)
     })
 })
+
+
+describe('tests for unsubscribe', () => {
+    test('should delete subscription', () => {
+        const subscriptions = [{endpoint:"https://foo"},{endpoint:"https://bar"}]
+        const req = {
+           query: {
+               subscription: 'http://foo'
+           }
+        }
+        const res = {
+            status: function status(code) {
+                this.status_code = code
+                return this
+            },
+            send: function send(res_body) {
+                this.body = res_body
+                return this
+            },
+        }
+        endpoints.unsubscribe(req, res, subscriptions)
+        expect(subscriptions.length).toBe(1)
+    })
+})
+
+
